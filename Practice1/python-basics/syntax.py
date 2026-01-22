9f0e3bdc09a68559169ff85aea90b7c58360db27
umbra17x <sanzhar1801@gmail.com>
2026-01-22 09:13:03 +0500

    Add Python Syntax examples fgj

---
+++ Practice1/python-basics/syntax.py
@@ -0,0 +1,14 @@
+# Example 1:Indentation
+if 5 > 2:
+  print("Five is greater than two!")
+# Example 2:Indentation
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+# Example 3:Variables
+x = 5
+y = "Hello, World!"
+# Example 4:Commments
+#This is a comment.
+print("Hello, World!")
