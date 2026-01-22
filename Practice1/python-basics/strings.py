379d541e0b32386d5f16a0c3f8ff17cad0cd4be8
umbra17x <sanzhar1801@gmail.com>
2026-01-22 19:13:35 +0500

    Add Python Strings examples from W3Schools

---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,22 @@
+#Example1
+print("Hello")
+print('Hello')
+#Example2
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+#Example3
+a = "Hello"
+print(a)
+#Example4
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+#Example5
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
