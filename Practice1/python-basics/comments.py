e7752242eedaebd35a0f816c462f0e60862e36b0
umbra17x <sanzhar1801@gmail.com>
2026-01-22 18:26:25 +0500

    Add Python Comments examples from W3Schools

---
+++ Practice1/python-basics/comments.py
@@ -0,0 +1,20 @@
+#Example1
+#This is a comment
+print("Hello, World!")
+#Example2
+print("Hello, World!") #This is a comment
+#Example3
+#print("Hello, World!")
+print("Cheers,Mate!")
+#Example4
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+#Example5
+"""
+This is a comment
+written in 
+more than just one line
+"""
+print("Hello, World!")
