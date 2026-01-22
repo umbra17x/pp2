7fb66ef86835524af8ddb910c74ba99f9993aac3
umbra17x <sanzhar1801@gmail.com>
2026-01-22 18:37:53 +0500

    Add Python Variables examples from W3Schools

---
+++ Practice1/python-basics/variables.py
@@ -0,0 +1,22 @@
+#Example1
+x = 5
+y = "John"
+print(x)
+print(y)
+#Example2
+x = 4       # x is not int
+x = "Sally" # x is now of type str
+print(x)
+#Example3
+x = str(3)   # x will be '3'
+y = int(3)   # y will be 3
+z = float(3) # z will be 3.0
+#Example4
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+#Example5
+x = "John"
+# is the same as
+x = 'John'
