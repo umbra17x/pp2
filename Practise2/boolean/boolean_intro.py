dcabe70b93ffaf97356f36f40b15905ba280a435
umbra17x <sanzhar1801@gmail.com>
2026-01-31 10:38:27 +0500

    Update boolean_intro examples

---
+++ Practise2/boolean/boolean_intro.py
@@ -0,0 +1,19 @@
+#Example1
+print(True)
+print(False)
+#Example2
+print(5>3)
+print(2 == 4)
+#Example3
+a = 10
+b = 10
+print(a == b)
+#Example4
+age = 18
+if age >= 18:
+    print("Adult")
+#Example5
+print(bool(1))
+print(bool(0))
+print(bool(""))
+print(bool("Hello"))
