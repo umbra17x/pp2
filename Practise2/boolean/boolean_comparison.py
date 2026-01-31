5606e06eca82ff773976c0d2094b6388b16915ac
umbra17x <sanzhar1801@gmail.com>
2026-01-31 11:01:37 +0500

    Add boolean_comparison examples

---
+++ Practise2/boolean/boolean_comparison.py
@@ -0,0 +1,22 @@
+#Example1 
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+#Example2
+a = 200
+b = 33
+print(a > b)
+print(a == b)
+#Example3
+print("apple" == "apple")
+print("apple" == "Apple")
+#Example4
+x = 5
+y = 5
+print(x != y)
+print(x >= y)
+#Example5
+x = 10
+y = 9
+if x > y:
+    print("x is greater than y")
