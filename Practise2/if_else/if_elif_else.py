dbcc3a5b483b4dd265deb7de781bfd6c540628cf
umbra17x <sanzhar1801@gmail.com>
2026-02-01 09:40:13 +0500

    Add if_else_elif examples

---
+++ Practise2/if_else/if_elif_else.py
@@ -0,0 +1,51 @@
+#Example1
+a = 33
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a anb b are equal")
+#Example2
+score = 75
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+#Example3
+age = 25
+if age < 13:
+    print("You are a child")
+elif age < 20:
+    print("You are a teenager")
+elif age < 65:
+    print("You are an adult")
+elif age >= 65:
+    print("You are a senior")
+#Example4
+day = 3
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+elif day == 7:
+    print("Sunday")
+#Example5
+x = 50
+if x > 100:
+    print("x is greater than 100")
+elif x > 30:
+    print("x is greater than 30")
+else:
+    print("x is 30 or less")
