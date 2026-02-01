b45fc470240cfb88a00771cc59bb67feb281faee
umbra17x <sanzhar1801@gmail.com>
2026-02-01 08:54:13 +0500

    Add if examples

---
+++ Practise2/if_else/if_statement.py
@@ -0,0 +1,25 @@
+#Example1 
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+#Example2
+number = 15
+if number > 0:
+    print("The number is positive")
+#Example3
+age = 20
+if age >= 18:
+    print("You are an adult")
+    print("You can vote")
+    print("You have full legal rights")
+#Example4
+a = 200
+b = 33
+if a > b: print("a is greater than b")
+#Example5
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+    print("Both conditions are True")
