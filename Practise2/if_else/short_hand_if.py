fab845e06d46cac3c16bfd53171957bcd5203231
umbra17x <sanzhar1801@gmail.com>
2026-02-01 10:32:43 +0500

    Add Short Hand If Else Examples

---
+++ Practise2/if_else/short_hand_if.py
@@ -0,0 +1,22 @@
+#Example1
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+#Example2
+a = 2
+b = 330
+print("A") if a > b else print("B")
+#Example3
+a = 10
+b = 20
+bigger = a if a > b else b
+print("Bigger is", bigger)
+#Example4
+a = 330
+b = 330
+print("A") if a > b else print("-") if a == b else print("B")
+#Example5
+x = 15
+y = 20
+max_value = x if x > y else y
+print("Maximum value:", max_value)
