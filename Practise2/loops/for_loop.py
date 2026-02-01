45b6d0e48a0fda52185ffa5e0d971c75e52bf798
umbra17x <sanzhar1801@gmail.com>
2026-02-01 19:03:40 +0500

    Add For Loops Examples

---
+++ Practise2/loops/for_loop.py
@@ -0,0 +1,16 @@
+#Example1
+fruits = ["apple", "banana" , "cherry"]
+for x in fruits:
+    print(x)
+#Example2
+for x in "banana":
+    print(x)
+#Example3
+for letter in "Hello":
+    print(letter)
+#Example4
+for i in range(3,6):
+    print(i)
+#Example5
+for i in range(1,10,2):
+    print(i)
