9e812f0abca255ea3cbf588c6379cf7826f0137d
umbra17x <sanzhar1801@gmail.com>
2026-02-01 10:44:38 +0500

    Add While Loops Examples

---
+++ Practise2/loops/while_loop.py
@@ -0,0 +1,25 @@
+#Example1
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+#Example2
+count = 1
+while count <= 3:
+    print("Hello")
+    count += 1
+#Example3
+num = 2
+while num <= 10:
+    print(num)
+    num += 2
+#Example4
+x = 5
+while x > 0:
+    print(x)
+    x -= 1
+#Example5
+x = 0
+while x < 3:
+    print("x is", x)
+    x += 1
