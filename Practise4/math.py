9b364f433a3269d22d38355b0196fe52ce8a49cc
umbra17x <sanzhar1801@gmail.com>
2026-02-21 12:43:59 +0500

    Add Practise4 - Python math

---
+++ Practise4/math.py
@@ -0,0 +1,24 @@
+#Example1(The min() and max() functions can be used to find the lowest or highest value in an iterable)
+x = min(5,10,25)
+y = max(5,10,25)
+print(x)
+#Example2(The abs() function returns the absolute(positive value of the specified number))
+x = abs(-7.25)
+print(x)
+#Example3(The pow(x,y) function returns the value of x to power of y (x^y))
+x = pow(4,3)
+print(x)
+#Example4(The math.sqrt() method returns the square root of a number)
+import math
+x = math.sqrt(64)
+print(x)
+#Example5(The math.ceil() method rounds a number upwards to its nearest integer, and the math.floor() method rounds a numbers downwards to its nearest integer, and returns the result)
+import math 
+x = math.ceil(1.4)
+y = math.floor(1.4)
+print(x)
+print(y)
+#Example6(The math.pi constant, returns the value of PI(3.14...))
+import math
+x = math.pi
+print(x)
